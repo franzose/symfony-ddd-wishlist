d97cf7d2e1bf3ce319ee7e5293c28519cf106b22
Jan Iwanow <iwanow.jan@gmail.com>
2017-07-16 22:15:05 +1100

    Added a couple of methods to interface of the wish repository

--- src/Domain/WishRepositoryInterface.php
+++ src/Domain/WishRepositoryInterface.php
@@ -4,5 +4,7 @@ namespace Wishlist\Domain;
 
 interface WishRepositoryInterface
 {
+    public function get(WishId $wishId): Wish;
+    public function put(Wish $wish);
     public function getNextWishId(): WishId;
 }
