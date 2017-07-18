cea5cab5adf921c7da41c1db000855dc7bf6c416
Jan Iwanow <iwanow.jan@gmail.com>
2017-07-18 20:35:22 +1100

    Added new methods to the WishRepositoryInterface

--- src/Domain/WishRepositoryInterface.php
+++ src/Domain/WishRepositoryInterface.php
@@ -7,5 +7,8 @@ interface WishRepositoryInterface
     public function get(WishId $wishId): Wish;
     public function put(Wish $wish);
     public function slice(int $offset, int $limit): array;
+    public function has(Wish $wish): bool;
+    public function hasWishWithId(WishId $wishId): bool;
+    public function count(): int;
     public function getNextWishId(): WishId;
 }
