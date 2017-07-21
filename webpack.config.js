0d030c38e9185bf66fd05ebd71df6fbee8cff776
Jan Iwanow <iwanow.jan@gmail.com>
2017-07-21 16:57:27 +1100

    Added basic resources for further development

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('web/assets/')
+    .setPublicPath('/assets')
+    .cleanupOutputBeforeBuild()
+    .addEntry('app', './app/Resources/assets/js/app.js')
+    .addStyleEntry('styles', './app/Resources/assets/scss/app.scss')
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
