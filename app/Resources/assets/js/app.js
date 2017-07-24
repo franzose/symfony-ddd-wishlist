cf30037a28fed7efef8f9d52f06f704da2966c9f
Jan Iwanow <iwanow.jan@gmail.com>
2017-07-24 17:28:45 +1100

    Added Publish/Unpublish button

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -1,1 +1,31 @@
-console.log('Yo!');
+;(function (window) {
+    window.addEventListener('load', function () {
+        let wishlist = document.getElementById('wishlist');
+
+        wishlist.addEventListener('click', function (event) {
+            let button = event.target;
+
+            if (!button.classList.contains('js-publish-button')) {
+                return;
+            }
+
+            let options = {
+                method: 'PUT'
+            };
+
+            fetch(button.dataset.url, options)
+                .then(response => response.json())
+                .then(function (response) {
+                    button.dataset.url = response.url;
+                    button.innerHTML = response.label;
+
+                    let method = response.published ? 'remove' : 'add';
+
+                    button.closest('.js-wish').classList[method]('wishlist__wish--unpublished');
+                })
+                .catch(function (e) {
+                    console.log(e);
+                });
+        });
+    });
+})(window);
