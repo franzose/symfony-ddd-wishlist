1a5fd647ed5903fbd04427c6268334c043183bbd
Jan Iwanow <iwanow.jan@gmail.com>
2017-08-03 17:06:32 +1100

    Added missing translation

--- app/Resources/assets/js/wish-deposits.js
+++ app/Resources/assets/js/wish-deposits.js
@@ -7,7 +7,7 @@ export default {
             <table class="wish-deposits__table table">
                 <caption class="table__caption">
                     <div class="table__caption-wrapper">
-                        <div class="table__caption-text">Deposits of ‘{{ wishName }}’</div>
+                        <div class="table__caption-text">{{ lang.depositsOf }} ‘{{ wishName }}’</div>
                         <svg @click="close" class="table__close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg>
                     </div>
                 </caption>
